4531fd318836b89f867cf2b05d6a1cca74fb37d2
AbdelfatahAtef <abdel.fattah@tajawal.com>
2019-11-26 12:47:58 +0400

    Add js docs to main js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+// To create a new node inside the document
 const createNode = (element) => {
     return document.createElement(element);
 }
 
+/**
+ * Append Element inside the parent element
+ * @param {*} parent 
+ * @param {*} element 
+ */
 const appendElement = (parent, element) => {
     return parent.appendChild(element);
 }
 
 const ul = document.getElementById('users');
 
+// To fetch users api and append this data inside the list
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(res => res.json())
 .then(users => {
